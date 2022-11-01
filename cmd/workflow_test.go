ebd77c179b23321add36749a3471a60433794a49
Mike Ball <mikedball@gmail.com>
2022-11-01 16:46:31 -0400

    DRY up JSON response strings

--- cmd/workflow_test.go
+++ cmd/workflow_test.go
@@ -28,7 +28,6 @@ func TestWorkflowDispatchRun(t *testing.T) {
 				workflow: workflow,
 			},
 			httpStubs: func(reg *httpmock.Registry) {
-				// TODO: DRY up redundant response JSON used by all tests
 				// TODO: test that proper request body is sent on POSTs
 				reg.Register(
 					httpmock.REST("POST", fmt.Sprintf("repos/%s/actions/workflows/%s/dispatches", repo, "workflow.yaml")),
@@ -39,16 +38,7 @@ func TestWorkflowDispatchRun(t *testing.T) {
 
 				reg.Register(
 					httpmock.QueryMatcher("GET", fmt.Sprintf("repos/%s/actions/runs", repo), v),
-					httpmock.StringResponse(`{
-						"total_count": 1,
-						"workflow_runs": [{
-							"id": 123,
-							"workflow_id": 456,
-							"name": "foo",
-							"status": "queued",
-							"conclusion": null
-						}]
-					}`))
+					httpmock.StringResponse(getWorkflowRunsResponse))
 
 				reg.Register(
 					httpmock.REST("GET", fmt.Sprintf("repos/%s/actions/runs/123", repo)),
@@ -68,52 +58,6 @@ func TestWorkflowDispatchRun(t *testing.T) {
 						"id": 123,
 						"status": "completed"
 					}`))
-
-				getJobsResponse := `{
-  					"total_count": 1,
-						"jobs": [{
-							"id": 123,
-							"run_id": 29679449,
-							"run_url": "https://api.github.com/repos/octo-org/octo-repo/actions/runs/29679449",
-							"node_id": "MDEyOldvcmtmbG93IEpvYjM5OTQ0NDQ5Ng==",
-							"head_sha": "f83a356604ae3c5d03e1b46ef4d1ca77d64a90b0",
-							"url": "https://api.github.com/repos/octo-org/octo-repo/actions/jobs/399444496",
-							"html_url": "https://github.com/octo-org/octo-repo/runs/399444496",
-							"status": "completed",
-							"conclusion": "success",
-							"started_at": "2020-01-20T17:42:40Z",
-							"completed_at": "2020-01-20T17:44:39Z",
-							"name": "build",
-							"steps": [
-								{
-									"name": "Run actions/checkout@v2",
-									"status": "completed",
-									"conclusion": "success",
-									"number": 2,
-									"started_at": "2020-01-20T09:42:41.000-08:00",
-									"completed_at": "2020-01-20T09:42:45.000-08:00"
-								},
-								{
-									"name": "Test",
-									"status": "completed",
-									"conclusion": "success",
-									"number": 3,
-									"started_at": "2020-01-20T09:42:45.000-08:00",
-									"completed_at": "2020-01-20T09:42:45.000-08:00"
-								}
-							],
-							"check_run_url": "https://api.github.com/repos/octo-org/octo-repo/check-runs/399444496",
-							"labels": [
-								"self-hosted",
-								"foo",
-								"bar"
-							],
-							"runner_id": 1,
-							"runner_name": "my runner",
-							"runner_group_id": 2,
-							"runner_group_name": "my runner group"
-						}]
-					}`
 
 				reg.Register(
 					httpmock.REST("GET", fmt.Sprintf("repos/%s/actions/runs/123/attempts/1/jobs", repo)),
